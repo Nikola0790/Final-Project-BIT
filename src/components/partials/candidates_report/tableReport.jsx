a39625b25cd5a70006af5999a419cd02cd45c891
Nikola0790 <nikola.drcalic@gmail.com>
2021-09-17 01:15:07 +0200

    getting data for modal

--- src/components/partials/candidates_report/tableReport.jsx
+++ src/components/partials/candidates_report/tableReport.jsx
@@ -2,6 +2,7 @@ import { Fragment } from "react";
 import { Modal } from "../single_report/single_report";
 
 export const TableReport = ({ dataReport }) => {
+  console.log(dataReport);
   return (
     <Fragment>
       <h3>Reports</h3>
@@ -10,18 +11,20 @@ export const TableReport = ({ dataReport }) => {
           <tr>
             <th scope="col">Company</th>
             <th scope="col">Interview Date</th>
-            <th scope="col">Status</th>
+            <th scope="col" colSpan="2">
+              Status
+            </th>
           </tr>
         </thead>
         <tbody>
-          {dataReport.map((item) => {
+          {dataReport.map((item, index) => {
             return (
               <tr>
                 <td>{item.companyName}</td>
                 <td>{item.interviewDate}</td>
                 <td>{item.status}</td>
                 <th scope="row">
-                  <Modal data={dataReport} />
+                  <Modal data={dataReport[index]} index={index} />
                 </th>
               </tr>
             );
--- src/components/partials/single_report/single_report.jsx
+++ src/components/partials/single_report/single_report.jsx
@@ -1,5 +1,7 @@
-export const Modal = ({ data }) => {
-  console.log(data);
+export const Modal = ({ data, index }) => {
+  let split = data.interviewDate.split(" ", 6);
+  console.log(split);
+  let date = `${split[2]}.${split[1]}.${split[3]}`;
   return (
     <div className="container">
       <div className="row">
@@ -8,18 +10,43 @@ export const Modal = ({ data }) => {
             type="button"
             className="btn btn-primary"
             data-toggle="modal"
-            data-target="#mymodal"
+            data-target={`#mymodal${index}`}
           >
-            eye
+            <i className="material-icons"> visibility </i>
           </button>
-          <div className="modal fade" id="mymodal">
+          <div className="modal fade" id={`mymodal${index}`}>
             <div className="modal-dialog">
               <div className="modal-content">
                 <div className="modal-header">
-                  <h3></h3>
+                  <h3>{data.candidateName}</h3>
                 </div>
                 <div className="modal-body">
-                  <p>Some content goes in here</p>
+                  <div className="container">
+                    <div className="row">
+                      <div className="col-4">
+                        <div className="row">
+                          <p>Company</p>
+                          <p>{data.companyName}</p>
+                        </div>
+                        <div className="row">
+                          <p>Interview date</p>
+                          <p>{date}</p>
+                        </div>
+                        <div className="row">
+                          <p>Phase</p>
+                          <p>{data.phase}</p>
+                        </div>
+                        <div className="row">
+                          <p>Status</p>
+                          <p>{data.status}</p>
+                        </div>
+                      </div>
+                      <div className="col-8">
+                        <p>Notes</p>
+                        <p>{data.note}</p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
                 <div className="modal-footer">
                   <button
