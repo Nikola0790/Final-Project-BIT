3c46e1ec2234e7d4128374be994dd46791ea4578
Nikola0790 <nikola.drcalic@gmail.com>
2021-09-18 11:13:39 +0200

    search bar centered, table report css

--- src/components/partials/candidates_report/tableReport.jsx
+++ src/components/partials/candidates_report/tableReport.jsx
@@ -1,36 +1,51 @@
 import { Fragment } from "react";
 import { Modal } from "../single_report/single_report";
+import "./tableReport.css";
 
 export const TableReport = ({ dataReport }) => {
   console.log(dataReport);
   return (
     <Fragment>
-      <h3>Reports</h3>
-      <table class="table table-striped">
-        <thead>
-          <tr>
-            <th scope="col">Company</th>
-            <th scope="col">Interview Date</th>
-            <th scope="col" colSpan="2">
-              Status
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {dataReport.map((item, index) => {
-            return (
-              <tr>
-                <td>{item.companyName}</td>
-                <td>{item.interviewDate}</td>
-                <td>{item.status}</td>
-                <th scope="row">
-                  <Modal data={dataReport[index]} index={index} />
-                </th>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <h3>Reports</h3>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <table class="table table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">Company</th>
+                  <th scope="col">Interview Date</th>
+                  <th scope="col" colSpan="2">
+                    Status
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {dataReport.map((item, index) => {
+                  let day = new Date(item.interviewDate).getDate();
+                  let month = new Date(item.interviewDate).getMonth() + 1;
+                  let year = new Date(item.interviewDate).getFullYear();
+                  let dateOfBirth = `${day}.${month}.${year}`;
+                  return (
+                    <tr>
+                      <td>{item.companyName}</td>
+                      <td>{dateOfBirth}</td>
+                      <td>{item.status}</td>
+                      <th scope="row">
+                        <Modal data={item} index={index} />
+                      </th>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </Fragment>
   );
 };
